d2c36a697e6751ebbe013c204b39e29ae8edc26e
Kouric-Calhoun <kouriccalhoun@gmail.com>
2021-06-04 19:43:37 -0400

    Added controller and templates

--- src/main/java/org/wecancoeit/reviews/Review.java
+++ src/main/java/org/wecancoeit/reviews/Review.java
@@ -9,6 +9,14 @@ public class Review {
         return id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
     public Review(long id, String name, String description) {
         this.id=id;
         this.name=name;
